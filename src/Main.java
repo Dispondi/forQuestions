84ac66cab6cb269246c1cc58159636c47144905e
Krytoi Xa4 <andrey.grachev.2007@mail.ru>
2024-10-23 20:57:31 +0300

    Question here

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
+
+        String numbers = sc.nextLine();
+        double result = 0;
+
+        String[] words = numbers.split(" + ");
+        for (String word : words) {
+            result += Double.parseDouble(word);
+            System.out.println(word);
+        }
+
+        System.out.println(result);
     }
 }
